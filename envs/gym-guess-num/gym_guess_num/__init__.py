620b0531c4c841b1c59456ec2024fd11de664fa4
Mitiku Yohannes <se.mitiku.yohannes@gmail.com>
2019-12-10 20:49:44 +0200

    Fix bug in registering the Environment

--- envs/gym-guess-num/gym_guess_num/__init__.py
+++ envs/gym-guess-num/gym_guess_num/__init__.py
@@ -1,10 +1,7 @@
 from gym.envs.registration import register
-
+from gym_guess_num.envs import GuessNumEnv,GuessNumExtraHardEnv
 register(
-    id='guess-num-v0',
+    id='GuessNumEnv-v0',
     entry_point='gym_guess_num.envs:GuessNumEnv',
-)
-register(
-    id='guess-num-extrahard-v0',
-    entry_point='gym_guess_num.envs:GuessNumExtraHardEnv',
+    kwargs = dict(num_digits = 4, repeat_digits=False, observable_size = 128)
 )
